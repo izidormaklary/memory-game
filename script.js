cad4c47d500dbf47478fb915846cceb6147c6eee
izidormaklary <maklaryizidor@gmail.com>
2021-04-30 19:05:14 +0200

    basic functionality, cards images ar displayed on click, and react on match

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 (function() {
-    let x;
+    let pairs =[];
     let cards = [];
-    let arrC = [];
+
     let card;
+    let checked = [];
+    let checkedId = [];
     const run = document.getElementById('run')
     const target = document.getElementById('wrap');
+    let allImg;
     let img = ['bike.png','c.png','car.png','chess-piece.png','d.png','h.png','ladder.png','s.png']
     run.addEventListener('click', ev => startGame())
     function startGame(){
@@ -19,7 +22,7 @@
             card= {data:i, image:img[i]}
             cards.push( card, card)
         }
-        console.log(cards)
+
     }
     function schuffleCards() {
         for (let i = 0; i < 16; i++) {
@@ -28,19 +31,20 @@
             cards[i] = cards[randindex];
             cards[randindex] = tempCard;
         }
-        console.log(cards)
+
     }
     function showCards(){
         target.innerHTML= '';
         for (f = 0; f < cards.length; f++) {
             let imgLink = "./resources/" + cards[f].image;
-            target.innerHTML = target.innerHTML + '<div class="cards" id="'+ f +'"> <img id="'+ f +'image" src="' + imgLink + '" alt="'+cards[i].image+'" width="70" height="70"> </div>';
+            target.innerHTML = target.innerHTML + '<div class="cards" id="'+ f +'"> <img id="'+ f +'image" src="' + imgLink + '" alt="'+cards[i].image+'" width="100px" height="100px"> </div>';
 
-           imagehide(f+'image');
+
 
             eventListen()
-
+            imagehide(f+"image")
         }
+
 
 
     }
@@ -53,9 +57,10 @@
         const cardList = document.querySelectorAll(".cards")
         cardList.forEach(card => {
             card.addEventListener('click', (ev)=>{
-                console.log(ev.currentTarget.id);
+
                 let id = ev.currentTarget.id
                 display(id+"image")
+                compare(id);
             })
         })
 
@@ -63,12 +68,53 @@
 
 
     }
+    function compare(id) {
+       checkedId.push(id);
+        checked.push(cards[id]);
+
+
+
+
+      if (checkedId.length > 1 && checkedId[checkedId.length-1] === checkedId[checkedId.length-2]){
+          checkedId.pop();
+          checked.pop();
+      } //else if ( )
+
+
+      if (checked.length === 2 &&  checked[0].data  !== checked[1].data ) {
+          let first =  checkedId[checkedId.length-2];
+          let second = checkedId[checkedId.length-1];
+
+            setTimeout(function (){
+                imagehide( first+"image");
+                imagehide( second+"image");
+            },800)
+
+            checked =[];
+            checkedId =[]
+      }else if (checked.length === 2 && checked[0] === checked[1]) {
+
+          pairs.push(checked.pop());
+          pairs.push(checked.pop());
+          let first = checkedId[0];
+          let sec   = checkedId[1]
+          changeclass(first);
+          changeclass(sec);
+          console.log( checkedId);
+          checkedId =[]
+      }
+
+    }
+function changeclass(id){
+    document.getElementById(id).className = "readycards";
+}
+
 
     function display(id){
 
-
         let img = document.getElementById(id)
-        img.style.display= "initial";
+        img.style.display= "block";
+
 
     }
 })();
